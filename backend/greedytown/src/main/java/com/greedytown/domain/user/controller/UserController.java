0743a33b5db282dae840f516c4d5952d71a424ef
sairo <from415@kakao.com>
2023-03-08 16:24:46 +0900

    [Feat] 회원 관리 : 기본 설정 중

--- backend/greedytown/src/main/java/com/greedytown/domain/user/controller/UserController.java
+++ backend/greedytown/src/main/java/com/greedytown/domain/user/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.greedytown.domain.user.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("")
+    public ResponseEntity<?> aliveCheck() {
+        return new ResponseEntity<>("Alive", HttpStatus.OK);
+    }
 }
