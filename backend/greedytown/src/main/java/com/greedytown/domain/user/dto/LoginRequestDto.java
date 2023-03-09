2f4b9ddb97ecefea078789d5f5710a71f8ecb6c8
sairo <from415@kakao.com>
2023-03-09 16:22:37 +0900

    [Feat] 로그인 구현 : 시큐리티, jwt, rest api 방식

--- backend/greedytown/src/main/java/com/greedytown/domain/user/dto/LoginRequestDto.java
+++ backend/greedytown/src/main/java/com/greedytown/domain/user/dto/LoginRequestDto.java
@@ -3,9 +3,8 @@ package com.greedytown.domain.user.dto;
 import lombok.Data;
 
 @Data
-public class UserDto {
+public class LoginRequestDto {
 
     private String userEmail;
     private String userPassword;
-    private String userNickname;
 }
---
+++ backend/greedytown/src/main/java/com/greedytown/global/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.greedytown.global.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greedytown.domain.user.dto.LoginRequestDto;
+import com.greedytown.global.config.auth.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    /**
+        인증 요청 시 실행되는 함수 (/login)
+    */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        // request에 있는 username과 password를 java Object로 받기
+        ObjectMapper om = new ObjectMapper();
+        LoginRequestDto loginRequestDto = null;
+        try {
+            loginRequestDto = om.readValue(request.getInputStream(), LoginRequestDto.class);
+            System.out.println(loginRequestDto.getUserEmail());
+            System.out.println(loginRequestDto.getUserPassword());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 유저네임패스워드 토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        loginRequestDto.getUserEmail(),
+                        loginRequestDto.getUserPassword());
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        return authentication;
+    }
+
+    /**
+        Authentication 객체가 성공적으로 만들어지면 JWT Token 생성해서 response에 담기
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        PrincipalDetails principalDetailis = (PrincipalDetails) authResult.getPrincipal();
+
+
+        String jwtToken = JWT.create()
+                .withSubject(principalDetailis.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME))
+                .withClaim("id", principalDetailis.getUser().getUserIndex())
+                .withClaim("username", principalDetailis.getUser().getUserEmail())
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+
+        System.out.println(jwtToken);
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX+jwtToken);
+    }
+}
