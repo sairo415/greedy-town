ae387b22d84cbe8774e4d1d8e8423fa15a1bec5b
sairo <from415@kakao.com>
2023-03-09 17:29:45 +0900

    [Fix] 회원가입 수정, request에 user 정보 저장

--- backend/greedytown/src/main/java/com/greedytown/global/config/jwt/JwtProperties.java
+++ backend/greedytown/src/main/java/com/greedytown/global/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.greedytown.global.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "greedy_town"; // 서버만 알고 있는 개인키
-    int EXPIRATION_TIME = 1000 * 60; // 1분
+    int EXPIRATION_TIME = 1000 * 60 * 60; // 1분
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
