86300a3df9ca20eab22f922e2604fd71cf56ae3a
sairo415 <from415@kakao.com>
2023-03-10 22:23:11 +0900

    [Fix] User 엔티티 userJoinDate 컬럼 타입 설정 추가

--- backend/greedytown/src/main/java/com/greedytown/domain/user/model/User.java
+++ backend/greedytown/src/main/java/com/greedytown/domain/user/model/User.java
@@ -24,6 +24,7 @@ public class User {
     private String userNickname;
     @Column(columnDefinition = "BIGINT DEFAULT = 0")
     private Long userMoney;
+    @Column(columnDefinition = "DATE")
     private Date userJoinDate;
 
 }
